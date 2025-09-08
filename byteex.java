1dcc52656d4ae4a71e59f30e4cc8f43f59862a11
Meet Parmar <parmarmeet3007@gmail.com>
2025-09-08 13:45:50 +0530

    Add files via upload

---
+++ byteex.java
@@ -0,0 +1,14 @@
+public class byteex {
+    public static void main(String[] args) {
+        byte smallno = 100;
+        byte minvalue = -128;
+        byte maxvalue = 127;
+        byte result = (byte) (smallno + 20);
+
+        system.out.println("Small number: " + smallno);
+        system.out.println("Minimum byte value: " + minvalue);
+        system.out.println("Maximum byte value: " + maxvalue);
+        system.out.println("Result Number: " + result);
+
+    }
+}
