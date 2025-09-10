7ad38aafe448ee6a9eafae63f2fc428695f90832
Meet Parmar <parmarmeet3007@gmail.com>
2025-09-10 14:54:59 +0530

    Add files via upload

---
+++ charex.java
@@ -0,0 +1,7 @@
+public class charex {
+    public static void main(String[] args) {
+        char first_vovel = 'A';
+        system.out.println("First vowel: " + first_vovel);
+        char second_vovel = 'E';
+    }
+}
